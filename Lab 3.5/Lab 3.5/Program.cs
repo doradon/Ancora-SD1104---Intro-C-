9e00e43f70c3bf570bd24361e66ffc2a2534e0d9
doradon <53684166+doradon@users.noreply.github.com>
2020-06-16 00:07:36 -0700

    Basic Scam Checker

---
+++ Lab 3.5/Lab 3.5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Lab_3._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string blackListWorld = "viagra";
+            bool isSpam = false;
+            Console.WriteLine("Please enter a message to check for spam:");
+            string message = Console.ReadLine();
+            if(message.Contains(blackListWorld))
+            {
+                isSpam = true;
+                Console.WriteLine("The message is spam");
+            }
+            else
+            {
+                isSpam = true;
+                Console.WriteLine("The message is not spam");
+            }
+        }
+    }
+}
