2798ed76fe4d3f616770c15b7a116031ca669fbb
doradon <53684166+doradon@users.noreply.github.com>
2020-06-16 00:09:40 -0700

    Using Arrays

---
+++ Lab 4.6/Lab 4.6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lab_4._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] days = {"Monday", "Tuesday", "Wednesday", "Thursday",
+                            "Friday", "Saturday", "Sunday"};
+            string[] tasks = new string[7];
+            int index = 0;
+
+            foreach (string day in days)
+            {
+                Console.WriteLine($"Type in a new Task for {day}");
+                tasks[index] = Console.ReadLine();
+
+                index++;
+            }
+
+            index = 0;
+            foreach (string day in days)
+            {
+                Console.WriteLine($"{day}: {tasks[index]}");
+
+                index++;
+            }
+        }
+    }
+}
