1cb1fbb086731aebd5c3381b171ceaaf8766fa2e
doradon <53684166+doradon@users.noreply.github.com>
2020-06-16 00:10:34 -0700

    Using Enums

---
+++ Lab 5.2/Lab 5.2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab_5._2
+{
+    class Program
+    {
+        public enum WeekDays
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+        static void Main(string[] args)
+        {
+            // Create a list to hold tasks
+            List<string> tasks = new List<string>();
+
+            // For each valiue in the enum.. let the user create a task
+            foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
+            {
+                Console.WriteLine("Enter a new task for " + day);
+                string task = day + " : " + Console.ReadLine();
+                tasks.Add(task);
+            }
+
+            // Print all of the tasks
+            foreach (string task in tasks)
+            {
+                Console.WriteLine(task);
+            }
+        }
+    }
+}
