3ad400c1c206e8489df7bc6228186099417134ca
doradon <53684166+doradon@users.noreply.github.com>
2020-06-17 14:40:02 -0700

    Using Inheritance (TV Show Application)

---
+++ Lab 8.5/Lab 8.5/ParadiseIsland.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_8._5
+{
+    class ParadiseIsland : TVApplication
+    {
+        private string gender;
+        private string nameOfSO;
+        private int yearsDating;
+
+        public ParadiseIsland (string gender, string nameOfSO, int yearsDating,
+                               string firstName, string lastName, string dateOfBirth,
+                               string address, string city, string state, int zip,
+                               string phoneNumber, string email) : base(firstName,
+                               lastName, dateOfBirth, address, city, state, zip,
+                               phoneNumber, email)
+        {
+            this.gender = gender;
+            this.nameOfSO = nameOfSO;
+            this.yearsDating = yearsDating;
+        }
+
+        public override void Accept()
+        {
+            base.Accept();
+            Console.WriteLine("You've been accepted to \"Paradise Island\"");
+        }
+    }
+}
