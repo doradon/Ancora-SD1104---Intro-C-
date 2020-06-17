4d5072e5acf0a6e9009554034edc949f97831785
doradon <53684166+doradon@users.noreply.github.com>
2020-06-16 22:41:33 -0700

    Using Classes (Dating Game)

---
+++ lab 7.4/lab 7.4/Messages.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab_7._4
+{
+    class Messages
+    {
+        public DatingProfile sender;
+        public string messageTitle;
+        public string messageData;
+        private bool _isRead;
+        public bool isRead
+        {
+            get { return _isRead; }
+            set
+            {
+                if (value == true | value == false)
+                {
+                    _isRead = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Incorrect reading status of message!");
+                }
+            }
+
+        }
+
+        public Messages(DatingProfile sender, string messageTitle, string messageData)
+        {
+            this.sender = sender;
+            this.messageTitle = messageTitle;
+            this.messageData = messageData;
+            this.isRead = false;
+        }
+    }
+}
