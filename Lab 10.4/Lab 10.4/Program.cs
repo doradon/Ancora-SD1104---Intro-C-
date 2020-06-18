5d2f626d159bb940764d66f536543e83d3b360eb
doradon <53684166+doradon@users.noreply.github.com>
2020-06-18 00:38:51 -0700

    Using Interfaces/Random (Media Player)

---
+++ Lab 10.4/Lab 10.4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lab_10._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Testing Artist Class
+            Artist prince = new Artist("Prince");
+            prince.AddToPlaylist("Purple Rain");
+            prince.AddToPlaylist("Little Red Corvette");
+            prince.AddToPlaylist("Kiss");
+            prince.AddToPlaylist("When Doves Cry");
+
+            MediaPlayer player = new MediaPlayer();
+            player.PlayMedia(prince);
+
+            // Testing AudioBook Class
+            AudioBook howto = new AudioBook("How to Win Friends & Influence People", "Dale Carnegie");
+            player.PlayMedia(howto);
+
+            // Testing MovieSoundTrack Class
+            MovieSoundTrack starWars = new MovieSoundTrack("Star Wars");
+            starWars.SelectTrack("Opening Credits Theme");
+            player.PlayMedia(starWars);
+        }
+    }
+}
