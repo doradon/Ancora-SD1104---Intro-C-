7844fce4c345b5c81494ccc5acbd0401e8dd91ac
doradon <53684166+doradon@users.noreply.github.com>
2020-06-17 18:07:21 -0700

    Using Inheritance (TV Show Applications)

---
+++ Lab 8.5/Lab 8.5/HouseHunter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_8._5
+{
+    class HouseHunter : TVApplication
+    {
+        private string jobTitle;
+        private int currentAnnualIncome;
+        private int whenBuyingHouseMonths;
+        private int numberOfBedrooms;
+        private int numberOfBathrooms;
+
+        public HouseHunter(string jobTitle, int currentAnnualIncome, 
+                           int whenBuyingHouseMonths, int numberOfBedrooms, 
+                           int numberOfBathrooms, string firstName, 
+                           string lastName, string dateOfBirth, 
+                           string address, string city, string state, int zip,
+                           string phoneNumber, string email) : base(firstName, 
+                           lastName, dateOfBirth, address, city, state, zip, 
+                           phoneNumber, email)
+        {
+            this.jobTitle = jobTitle;
+            this.currentAnnualIncome = currentAnnualIncome;
+            this.whenBuyingHouseMonths = whenBuyingHouseMonths;
+            this.numberOfBedrooms = numberOfBedrooms;
+            this.numberOfBathrooms = numberOfBathrooms;
+        }
+        public override void Accept()
+        {
+            base.Accept();
+            Console.WriteLine("You've been accepted to \"Dream House Hunters\"");
+        }
+    }
+}
---
+++ Lab 8.5/Lab 8.5/TVApplication.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_8._5
+{
+    class TVApplication
+    {
+        protected string firstName;
+        protected string lastName;
+        protected string dateOfBirth;
+        protected string address;
+        protected string city;
+        protected string state;
+        protected int zip;
+        protected string phoneNumber;
+        protected string email;
+        protected bool applicationSubmitted;
+        protected bool applicationAccepted;
+
+        // TVApplication constructor
+        public TVApplication(string firstName, string lastName, string dateOfBirth,
+                           string address, string city, string state, int zip,
+                           string phoneNumber, string email)
+        {
+            // Base properties
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.dateOfBirth = dateOfBirth;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+            this.applicationSubmitted = false;
+            this.applicationAccepted = false;
+        }
+
+        // Submit () will set Application Submitted to true and print to console
+        // “Application has been submitted.”
+        public void Submit()
+        {
+            this.applicationSubmitted = true;
+            Console.WriteLine("Application has been submitted");
+        }
+        // Accept() will set the application to accepted and print “Application 
+        // Accepted”.
+        public virtual void Accept()
+        {
+            this.applicationAccepted = true;
+            Console.WriteLine("Application Accepted");
+        }
+    }
+}
